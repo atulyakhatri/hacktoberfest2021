c31d1503f514d64be17b17dd5ea616cceb83d311
atulyakhatri <65857938+atulyakhatri@users.noreply.github.com>
2021-10-04 17:36:01 +0530

    Revert "Create car.py"

--- Python/car.py
+++ Python/car.py
@@ -1,28 +1,23 @@
-#IMPORTING MODULES
 import pygame
 
 pygame.init()
-
-#Constants
-GRAY = (119, 118, 110)
-DISPLAY_WIDTH = 800
-DISPLAY_HEIGHT = 600
-
-GAME_DISPLAYS = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
-pygame.display.set_caption("car game")
-clock = pygame.time.Clock()
+gray=(119,118,110)
+display_width=800
+display_height=600
+gamedisplays=pygame.desplay.set_mode((display_width,display_height))
+pygame.displya.set_caption("car game")
+clock=pygame.time.Clock()
 
 def game_loop():
-    bumped = False
+    bumped=False
     while not bumped:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                bumped = True
+        for event in python.event.get():
+            if event.type==pygame.QUIT:
+                bumped=True
 
-        GAME_DISPLAYS.fill(GRAY)
+        gamedisplays.fill(gray)
         pygame.display.update()
-        clock.tick(2)
-
+        clock.tick(60)
 game_loop()
 pygame.quit()
 quit()
